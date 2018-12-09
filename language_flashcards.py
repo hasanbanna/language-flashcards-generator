4b5385482bec8feb8e4ea47eb7895c4475acb097
Hasan Banna <banna.hsn@gmail.com>
2018-12-09 15:56:37 +0000

    refactored code

--- language_flashcards.py
+++ language_flashcards.py
@@ -6,30 +6,23 @@ from google.cloud import texttospeech
 from google.cloud import translate
 
 import six
-import argparse
+# Might use argparse to create command line tool
 from google_images_download import google_images_download
-
 response = google_images_download.googleimagesdownload()
 
-# parser = argparse.ArgumentParser()
-# parser.add_argument('-text', "--text", help="input text")
-# parser.add_argument("-tlang", "--target_language", help="target language")
-# args = parser.parse_args()
+
 
 # target_language = args.target_language
-text = "Je trouve leur maison très belle, mais ils ne sont pas contents et ils cherchent une autre maison à la campagne, avec des chambres individuelles pour les enfants"
 
-"""
-For each word create a text to speech pronunciation along with the pronunciation of the sentence aswell as an extra.
-"""
-def generate_cloze_sentences(sentence, clozed_words):
+
+def generate_cloze_deletion(sentence, clozed_words):
     cloze_sentences = []
     for word in sentence.split(" "):
         if(word in clozed_words):
             cloze_sentences.append(sentence.replace(word,"{{c1::%s}}"%(word)))
     return cloze_sentences
 
-def get_relevant_pos_from_text(text):
+def get_relevant_pos_data_from_text(text):
     """Detects syntax in the text."""
     client = language.LanguageServiceClient()
 
@@ -54,15 +47,18 @@ def get_relevant_pos_from_text(text):
 
     relevant_content = []
     for token in tokens:
+        pos_data = {}
         if pos_tag[token.part_of_speech.tag] in relevant_tags:
             if(token.part_of_speech.proper == 1): # if proper noun(1) then skip
                 continue
             if(token.text.content not in relevant_content):
-                relevant_content.append(token.text.content)
-            # print (gender[token.part_of_speech.gender], token.text.content)
+                pos_data['gender'] = gender[token.part_of_speech.gender]
+                pos_data['tag'] = pos_tag[token.part_of_speech.tag]
+                pos_data['word'] = token.text.content
+                relevant_content.append(pos_data)
     return relevant_content
 
-def name_entities_from_text(text):
+def name_entities_from_sentence(text):
     """Detects entities in the text."""
     client = language.LanguageServiceClient()
 
@@ -88,47 +84,52 @@ def name_entities_from_text(text):
             entities_names.append(entity.name)
     return entities_names
 
-
-
 # fr-FR
-def generate_speech_from_text(langauge_code, speaking_rate, filename):
+def generate_speech_from_text(lang, speaking_rate, filename, txt):
+    if(len(filename) >= 15):
+        filename = filename[:15] + "_" + lang
     # Instantiates a client
-    client = texttospeech.TextToSpeechClient()
+    # client = texttospeech.TextToSpeechClient()
 
-    # Set the text input to be synthesized
-    synthesis_input = texttospeech.types.SynthesisInput(text=text)
+    # # Set the text input to be synthesized
+    # synthesis_input = texttospeech.types.SynthesisInput(text=txt)
 
-    # Build the voice request, select the language code ("en-US") and the ssml
-    # voice gender ("neutral")
-    voice = texttospeech.types.VoiceSelectionParams(
-        language_code=language_code,
-        ssml_gender=texttospeech.enums.SsmlVoiceGender.FEMALE)
+    # # Build the voice request, select the language code ("en-US") and the ssml
+    # # voice gender ("neutral")
+    # voice = texttospeech.types.VoiceSelectionParams(
+    #     language_code=lang,
+    #     ssml_gender=texttospeech.enums.SsmlVoiceGender.FEMALE)
 
-    # Select the type of audio file you want returned
-    audio_config = texttospeech.types.AudioConfig(
-        audio_encoding=texttospeech.enums.AudioEncoding.MP3,
-        speaking_rate=speaking_rate)
+    # # Select the type of audio file you want returned
+    # audio_config = texttospeech.types.AudioConfig(
+    #     audio_encoding=texttospeech.enums.AudioEncoding.MP3,
+    #     speaking_rate=speaking_rate)
 
-    # Perform the text-to-speech request on the text input with the selected
-    # voice parameters and audio file type
-    response = client.synthesize_speech(synthesis_input, voice, audio_config)
+    # # Perform the text-to-speech request on the text input with the selected
+    # # voice parameters and audio file type
+    # response = client.synthesize_speech(synthesis_input, voice, audio_config)
 
     # The response's audio_content is binary.
-    with open('audio/'+filename+'.mp3', 'wb') as out:
-        # Write the response to the output file.
-        out.write(response.audio_content)
-        print('Audio content written to file "output.mp3"')
+    # with open('{}.mp3'.format(filename), 'wb') as out:
+    #     # Write the response to the output file.
+    #     out.write(response.audio_content)
+    #     print('Audio content written to file {}'.format(filename))
+    # [sound:vocab13en.mp3]
+    return"[sound:{}.mp3]".format(filename)
 
-# print(name_entities_from_text(text))
-
-clozed_words = get_relevant_pos_from_text(text)
+# TODO: For each entity create an image
+# TODO: For each word create a TTS
+# TODO: For each sentence create a TTS
+# TODO: For each sentece createa translation
 # arguments = {"keywords": ','.join(clozed_words), "limit":1, "format":'png'}
 # paths = response.download(arguments)
 # print(paths)
-clozed_sentences = generate_cloze_sentences(text, clozed_words)
-# print(clozed_sentences)
-for cs in clozed_sentences:
-    print(cs)
+
+def get_words(words_dict):
+    words = []
+    for pos_data in words_dict:
+        words.append(pos_data['word'])
+    return words
 
 def translate_text(text, target_language):
     # Instantiates a client
@@ -136,4 +137,24 @@ def translate_text(text, target_language):
     target = target_language
     translation = translate_client.translate(text, target_language=target)
     return translation['translatedText']
-print(translate_text(text, 'en'));
+
+def generate_images(context_words):
+    context_word_images_name = []
+    for word in context_words:
+        context_word_images.append(word+'.png')
+    return context_word_images_name
+
+def generate_audio_file_names(clozed_words):
+    audio_file_names = []
+    for word in clozed_words:
+        audio_file_names.append("[sound:{}.mp3]".format(word['word']))
+    print ('audio files: ', audio_file_names)
+
+# Translate the target language to source language
+def translate_text(text, target_language):
+    # Instantiates a client
+    translate_client = translate.Client()
+    target = target_language
+    translation = translate_client.translate(text, target_language=target)
+    return translation['translatedText']
+
--- main.py
+++ main.py
@@ -1,20 +1,39 @@
 from language_flashcards import *
 
+# Assumptions
+# Each word is unique
+# Each word is processed in order
+
 print ('{}\t\t\t{}'.format('front', 'back'))
 print ('{}\t\t\t{}'.format('Je ... leur maison', 'trouve "context_image" "word_audio" "sentence_audio" '))
-
 
 # Get target language
 source_langauge = 'en'
 # Get source language
 target_language = 'fr-FR'
 
-target_language_text = "Je trouve leur maison très belle, mais ils ne sont pas contents et ils cherchent une autre maison à la campagne, avec des chambres individuelles pour les enfants"
+target_language_sentence = "Je trouve leur maison très belle, mais ils ne sont pas contents et ils cherchent une autre maison à la campagne, avec des chambres individuelles pour les enfants"
 
 # translate the text into source langauge - i.e French -> English
-translated_into_source_text = translate_text(target_language_text, source_langauge)
+translated_into_source_text = translate_text(target_language_sentence, source_langauge)
 
 # Get part of speech from the sentences to be used for clozed deletion
-clozed_words_from_pos_data = get_relevant_pos_data_from_text(text)
-# Create clozed sentences with the target language text
+clozed_words_from_pos_data = get_relevant_pos_data_from_text(target_language_sentence)
 
+# Create clozed sentences with the target language text
+clozed_sentences = generate_cloze_deletion(target_language_sentence, clozed_words)
+
+# Get named entities from the sentence
+named_entities = name_entities_from_sentence(target_language_sentence)
+
+# for each entity generate a contextual image
+print("---Generating image files---")
+named_entities_images = generate_images(named_entities)
+
+#  generate an audio for the sentence for each clozed word
+print("---Generating audio files---")
+generate_speech_from_text('fr-FR', 0.8, target_language_sentence, target_language_sentence)
+for word in get_words(clozed_words):
+  generate_speech_from_text('fr-FR', 0.8, word, word)
+
+# Clozed Sentence	Context Image Word Audio Sentence Audio English Translation Part of Speech
